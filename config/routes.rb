3da9f4ac50ff0c134325e02f17e0fbf80e3bc370
tkorthal <tkorthal@brotherhoodmutual.com>
2015-10-23 14:26:18 -0400

    Refactor views, add List scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :lists
   resources :items
   root to: 'items#index' 
   # The priority is based upon order of creation: first created -> highest priority.
